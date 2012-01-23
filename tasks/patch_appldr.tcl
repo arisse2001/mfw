60152a68f65cdd851a4b5130e1e083538674cff5
arisse2001 <arisse2001@hotmail.com>
2012-01-23 17:56:23 +0100

    Update tasks/patch_appldr.tcl

--- tasks/patch_appldr.tcl
+++ tasks/patch_appldr.tcl
@@ -9,19 +9,16 @@
 #
 
 # Priority: 300
-# Description: Patch Appldr (Experimental!)
+# Description: Patch Appldr
 
-# Option --patch-appldr-fself-340: Patch Appldr to allow Fself (3.40-3.55) set debug true
-# Option --patch-appldr-fself-330: Patch Appldr to allow Fself (3.10-3.30) set debug true
+# Option --patch-appldr-fself: Patch Appldr to allow Fself (set debug true)
 
-# Type --patch-appldr-340: boolean
-# Type --patch-appldr-330: boolean
+# Type --patch-appldr: boolean
 
 namespace eval ::patch_appldr {
 
     array set ::patch_appldr::options {
-        --patch-appldr-fself-340 false
-        --patch-appldr-fself-330 true
+        --patch-appldr-fself true
     }
 
     proc main { } {
@@ -30,9 +27,17 @@ namespace eval ::patch_appldr {
         ::modify_coreos_file $self ::patch_appldr::patch_self
     }
 
+    proc patch_self {self} {
+        if {!$::patch_appldr::options(--patch-appldr-fself)} {
+            log "WARNING: Enabled task has no enabled option" 1
+        } else {
+            ::modify_self_file $self ::patch_appldr::patch_elf
+        }
+    }
+
     proc patch_elf {elf} {
-        if {$::patch_appldr::options(--patch-appldr-fself-340)} {
-            log "Patching Appldr to allow Fself (3.40-3.55)"
+        if {$::patch_appldr::options(--patch-appldr-fself)} {
+            log "Patching Appldr to allow Fself"
 
             set search  "\x40\x80\x0e\x0c\x20\x00\x57\x83\x32\x00\x04\x80\x32\x80\x80"
             set replace "\x40\x80\x0e\x0c\x20\x00\x57\x83\x32\x11\x73\x00\x32\x80\x80"
@@ -41,17 +46,4 @@ namespace eval ::patch_appldr {
                 "Unable to patch self [file tail $elf]"
         }
     }
-
-    proc patch_elf {elf} {
-        if {$::patch_appldr::options(--patch-appldr-fself-330)} {
-            log "Patching Appldr to allow Fself (3.10-3.30)"
-
-            set search  "\x40\x80\x0e\x0d\x20\x00\x69\x09\x32\x00\x04\x80\x32\x80\x80"
-            set replace "\x40\x80\x0e\x0c\x20\x00\x57\x83\x32\x11\x73\x00\x32\x80\x80"
-
-            catch_die {::patch_elf $elf $search 7 $replace} \
-                "Unable to patch self [file tail $elf]"
-        }
-    }
-
 }
